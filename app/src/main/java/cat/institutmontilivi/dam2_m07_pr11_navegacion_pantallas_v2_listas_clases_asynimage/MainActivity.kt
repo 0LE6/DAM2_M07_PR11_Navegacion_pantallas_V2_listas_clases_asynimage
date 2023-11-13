334fc2f34f80a832c18e1ff08717177401f934fe
Oleg <kharenkoleg@gmail.com>
2023-11-13 16:22:34 +0100

    COMM --

--- app/src/main/java/cat/institutmontilivi/dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage/MainActivity.kt
+++ app/src/main/java/cat/institutmontilivi/dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello QWERTY $name!",
         modifier = modifier
     )
 }
