da6d558f27a414d6d14c41664c961982e4f771ec
Oleg <kharenkoleg@gmail.com>
2023-11-15 03:24:34 +0100

    COMM -- avanzando

--- app/src/main/java/cat/institutmontilivi/dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage/Datos/Guerreros.kt
+++ app/src/main/java/cat/institutmontilivi/dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage/Datos/Guerreros.kt
@@ -1,2 +1,52 @@
 package cat.institutmontilivi.dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage.Datos
 
+import androidx.compose.ui.graphics.Color
+
+data class Guerrero(
+    val id : Int,
+    val nombre : String,
+    val foto : String, // dirección de la imagen
+    val color : Color,
+    val edad : Int,
+    val fuerza : Int,
+    val resistencia : Int,
+    val ataque : Int,
+    val defensa : Int
+)
+
+class Guerreros {
+    companion object {
+
+        public val datos =
+            listOf(
+                Guerrero(1,
+                    "Espada misteriosa",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/300px-Bandera_de_España.svg.png",
+                    Color.Blue,
+                    23, 56, 45, 69, 89
+                ),
+                Guerrero(1,
+                    "Espasa misteriosa",
+                    "https://loremflickr.com/300/300/warrior?lock=27",
+                    Color.Blue,
+                    23, 56, 45, 69, 89),
+                Guerrero(1,
+                    "Espasa misteriosa",
+                    "https://loremflickr.com/300/300/warrior?lock=28",
+                    Color.Blue,
+                    23, 56, 45, 69, 89),
+                Guerrero(1,
+                    "Espasa misteriosa",
+                    "https://loremflickr.com/300/300/warrior?lock=29",
+                    Color.Blue,
+                    23, 56, 45, 69, 89),
+                Guerrero(1,
+                    "Espasa misteriosa",
+                    "https://loremflickr.com/300/300/warrior?lock=30",
+                    Color.Blue,
+                    23, 56, 45, 69, 89)
+
+            )
+
+    }
+}
--- app/src/main/java/cat/institutmontilivi/dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage/ui/Pantallas/PantallaListaDeGuerreros.kt
+++ app/src/main/java/cat/institutmontilivi/dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage/ui/Pantallas/PantallaListaDeGuerreros.kt
@@ -1,2 +1,51 @@
 package cat.institutmontilivi.dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage.ui.Pantallas
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import cat.institutmontilivi.dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage.Datos.Guerrero
+import cat.institutmontilivi.dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage.Datos.Guerreros
+import cat.institutmontilivi.dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage.R
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import androidx.compose.foundation.lazy.items // Importante para los items de LazyColumn
+@Composable
+fun PantallaListaDeGuerreros(titulo : String = "Titulo") {
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        item { Text (text = titulo) }
+        items(Guerreros.datos) { guerrero ->
+            GuerreroItem(guerrero)
+        }
+    }
+}
+
+@Composable
+fun GuerreroItem(guerrero: Guerrero) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(guerrero.foto)
+            .crossfade(true)
+            .build(),
+        placeholder = painterResource(R.drawable.ic_launcher_foreground),
+        contentDescription = "Super Guerrer",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.clip(CircleShape)
+    )
+    Spacer(modifier = Modifier.padding(8.dp))
+
+    val valor = guerrero.id //obtener la id o el indice del campo para mandarlo por parametro
+}
