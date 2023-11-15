480675f694d89aaa8cd67589338461a8855f7acc
Oleg <kharenkoleg@gmail.com>
2023-11-15 02:26:51 +0100

    COMM -- avanzando

--- app/src/main/java/cat/institutmontilivi/dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage/ui/Pantallas/PantallaPrincipal.kt
+++ app/src/main/java/cat/institutmontilivi/dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage/ui/Pantallas/PantallaPrincipal.kt
@@ -1,2 +1,12 @@
 package cat.institutmontilivi.dam2_m07_pr11_navegacion_pantallas_v2_listas_clases_asynimage.ui.Pantallas
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PantallaPrincipal(
+    onListaDeGuerreros : (String) -> Unit,
+    onListaDeCoches : (String) -> Unit,
+    onListaDePaises : (String) -> Unit
+) {
+
+}
